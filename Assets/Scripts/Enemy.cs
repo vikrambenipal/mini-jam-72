ea87e422307686f03a8f5a27a284f410acfb6a33
vikrambenipal <vikrambenipal@gmail.com>
2021-01-22 16:09:16 -0500

    Prototype for player capturing enemies works kind of

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public bool isAlive;
+    public bool isPossesed;
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+
+    private void Start()
+    {
+        isAlive = true;
+        isPossesed = false;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("works");
+        if(other.tag == "Player")
+        {
+            other.gameObject.SetActive(false);
+            isPossesed = true;
+        }
+        
+    }
+
+    private void Update()
+    {
+        if (!isPossesed)
+        {
+
+        }
+        else
+        {
+            rb.velocity = new Vector2(moveSpeed * Input.GetAxisRaw("Horizontal"), rb.velocity.y);
+        }
+    }
+
+
+}
