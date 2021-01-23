b773b276f5f24550f3dee25483bd34161a1649c6
vikrambenipal <vikrambenipal@gmail.com>
2021-01-23 01:18:20 -0500

    when a captured enemy dies, player ghost respawns

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public void KillPlayer()
+    {
+        FindObjectOfType<PlayerController>().isDead = true;
+    }
+}
---
+++ Assets/Scripts/Spike.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spike : MonoBehaviour
+{
+    public PlayerController pc;
+
+    private void Awake()
+    {
+        pc = FindObjectOfType<PlayerController>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player" || LayerMask.LayerToName(other.gameObject.layer) == "EnemyCapture")
+        {
+            pc.isDead = true;
+
+            if(LayerMask.LayerToName(other.gameObject.layer) == "EnemyCapture")
+            {
+                pc.gameObject.SetActive(true);
+                pc.transform.position = other.transform.position;
+                other.gameObject.SetActive(false);
+            }
+        }
+    }
+}
